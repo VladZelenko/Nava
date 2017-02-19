58cd6cdcbf9e9f974f86fc7d85e52da4cd0876fb
VladZelenko <Vlad6555@ukr.net>
2017-02-19 14:48:07 +0200

    add popup on section

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -39,12 +39,22 @@ $(document).ready(function() {
 
 //uploading img x3
 function showImages(){
-	var images = $('img.hide')
+	var images = $('.hide')
 	if (images.length == 0){
-		$('button#galery-btn').hide();
+		$('button#galery-btn').hide(2000);
 	}
 	else {
-		$('img.hide').slice(0,3).removeClass('hide')
+		$('.hide').slice(0,3).removeClass('hide')
+	}
+}
+
+function showImagesPrice(){
+	var images = $('.hide-up')
+	if (images.length == 0){
+		$('button#galery-price-btn').hide(2000);
+	}
+	else {
+		$('.hide-up').slice(0,3).removeClass('hide-up')
 	}
 }
 
@@ -68,3 +78,32 @@ function showImages(){
 // if (a.length == 0) {
 // 	$('.up-btn').css({"display" : "none"})
 // }
+//
+$(document).ready(function(){
+	$('a.poplight[href*=\\#]').click(function() {
+		var popID = $(this).attr('rel');
+		var popURL = $(this).attr('href');
+		var query= popURL.split('?');
+		var dim= query[1].split('&');
+		var popWidth = dim[0].split('=')[1];
+
+		$('#' + popID).fadeIn().css({ 'width': Number( popWidth ) }).prepend('<a href="#" title="Закрыть" class="close"></a>');
+		var popMargTop = ($('#' + popID).height() + 80) / 2;
+		var popMargLeft = ($('#' + popID).width() + 80) / 2;
+
+		$('#' + popID).css({
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+
+		$('body').append('<div id="fade"></div>');
+		$('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn();
+		return false;
+	});
+        $(document).on('click', 'a.close, #fade', function() {
+        $('#fade , .popup_block').fadeOut(function() {
+        $('#fade, a.close').remove();
+    });
+    return false;
+   });
+});
